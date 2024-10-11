be0ea95c1ce237ff5bd4e3437a90212e2545fc41
Nicolas Blin <nicolas.blin@banookgroup.com>
2024-10-11 15:10:21 +0200

    feat: pipe-solution

--- src/app/todo/todo-list.component.ts
+++ src/app/todo/todo-list.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LoggerDirective } from '../directives/logger.directive';
+import { FromNowPipe } from '../pipes/from-now.pipe';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   imports: [
     RouterLink,
-    LoggerDirective
+    LoggerDirective,
+    FromNowPipe
   ],
   template: `
     <a routerLink="/home">go to home</a>
-    <div class="loggable" logText>Hello</div>
-    <div class="loggable" logText notLoggable="true">Hello</div>
+    {{ date | fromNow }}
   `,
   styles: ``
 })
-export default class TodoListComponent {}
+export default class TodoListComponent {
+  date = '2025-02-11T11:30:30';
+}
