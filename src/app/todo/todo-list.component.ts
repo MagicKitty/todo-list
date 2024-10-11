d022a7e33c62a577c55abcb2980820b4f189f153
Nicolas Blin <nicolas.blin@banookgroup.com>
2024-10-11 14:44:01 +0200

    feat: directive-solution

--- src/app/todo/todo-list.component.ts
+++ src/app/todo/todo-list.component.ts
@@ -11,6 +11,8 @@ import { LoggerDirective } from '../shared/logger.directive';
   ],
   template: `
     <a routerLink="/home">go to home</a>
+    <div class="loggable" logText>Hello</div>
+    <div class="loggable" logText notLoggable="true">Hello</div>
   `,
   styles: ``
 })
