33217b88cf63d528ab71a77c7bc7fc3f46e346a2
Nicolas Blin <nicolas.blin@banookgroup.com>
2024-10-11 10:27:38 +0200

    feat: component solution

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-todo',
@@ -6,7 +6,44 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     todo component works!
+    <br />
+    <span>item {{ rankTest }}: {{ nameTest }}</span>
+    <br />
+    <span>item {{ item.rank }}: {{ item.name }}</span>
+    <br />
+    <span>item {{ item2?.rank }}: {{ item2?.name }}</span>
+    <br />
+    <span>item {{ rank() }}: {{ name() }}</span>
+    <br />
+    <button (click)="click($event)">click</button>
+    <br />
+    @if (rank() && name()) {
+      <span>item {{ rank() }}: {{ name() }}</span>
+    }
+    <br />
+    @for (element of elements; track $index) {
+      {{ element }}
+    }
+    <br />
   `,
   styles: ``
 })
-export class TodoComponent {}
+export class TodoComponent {
+  private readonly a = 'red'; // ERRORS in template if called as interpolation
+  readonly rankTest = 1;
+  readonly nameTest = 'pasta';
+  readonly item = {
+    name: 'tofu',
+    rank: '2'
+  };
+  readonly item2: { name: string, rank: number } | null | undefined;
+
+  name = input<string>();
+  rank = input<number>();
+
+  click(event: MouseEvent) {
+    console.log(event);
+  }
+
+  elements = [5, 6, 7]
+}
