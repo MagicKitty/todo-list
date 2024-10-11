a8a8b7da296bbec237aafa4dea8b8d0f4153d988
Nicolas Blin <nicolas.blin@banookgroup.com>
2024-10-11 14:44:01 +0200

    feat: directive-solution

--- src/app/shared/logger.directive.ts
+++ src/app/shared/logger.directive.ts
@@ -1,8 +1,11 @@
 import { Directive } from '@angular/core';
 
 @Directive({
-  selector: '',
+  selector: 'div.loggable[logText]:not([notLoggable=true])',
   standalone: true
 })
 export class LoggerDirective {
+  constructor() {
+    console.log('Complex selector directive');
+  }
 }
